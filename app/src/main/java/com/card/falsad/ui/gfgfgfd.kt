0af7e9a301378a0131ceb230e18e8a87e60bd090
vlad <vik1vik007@gmail.com>
2022-01-19 23:34:03 +0200

    fsddsffdsfsd

---
+++ app/src/main/java/com/card/falsad/ui/gfgfgfd.kt
@@ -0,0 +1,6 @@
+package com.card.falsad.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.card.falsad.R
+
+class gfgfgfd : AppCompatActivity(R.layout.dfsdfsgsd)
---
+++ app/src/main/java/com/card/falsad/util/dfsfdsgsgfd.kt
@@ -0,0 +1,5 @@
+package com.card.falsad.util
+
+import android.view.View
+
+fun View.getDrawableId(name: String) = context.getDrawableId(name)
